f23723c3c602f00cd6bc04638fc2d6c57e2b5ac9
Emilio <planeta.chompi18@gmail.com>
2023-11-19 10:22:06 +0800

    learning

---
+++ vueCLI/src/components/Modal.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import { ref } from 'vue'
+    
+    const show = ref(false);
+
+    function toggle(){
+        show.value = !show.value  
+    }
+
+
+</script>
+
+<template>
+
+    <div>
+        <button @click="toggle">
+            Modal</button>
+            <Teleport to="body">
+            <div v-show="show" class="modal">
+                <h1>Titulo</h1>
+                <p>Lorem ipsum dolor</p>
+                <button @click="toggle">Cerrar</button>
+            </div>
+        </Teleport>
+        
+    </div>
+
+</template>
